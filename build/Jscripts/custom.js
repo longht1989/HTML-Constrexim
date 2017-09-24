f8de72c37e0b70934c019fbbc89a0c9e5d3667f9
longht1989 <thanhlong.vn09@gmail.com>
2017-09-25 03:38:53 +0700

    update demo Constrexim

---
+++ build/Jscripts/custom.js
@@ -0,0 +1,31 @@
+/* ====================================
+   Onload functions
+   ==================================== */
+
+$(function() {
+    $('.search-wrap .fa-search').click(function(){
+     $(this).parent( ".search-wrap").toggleClass("is-active");
+    });
+    $('#featured-slider ul').bxSlider({
+        prevText: '',
+        nextText: ''
+    });
+    $('#project ul').bxSlider({
+        prevText: '',
+        nextText: '',
+        nextSelector: '#project-next1',
+        prevSelector: '#project-prev1',
+        pager: 0
+    });
+    // jquery ui slider range for price
+    $("#filter-price").slider({
+        range: true,
+        min: 500,
+        max: 10000,
+        values: [1500, 8000],
+        slide: function(event, ui) {
+            $(".amount-price").val(ui.values[0] + " $ - " + ui.values[1] + " $");
+        }
+    });
+    $(".amount-price").val($("#filter-price").slider("values", 0) + " $ - " + $("#filter-price").slider("values", 1) + " $");
+});
